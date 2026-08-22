4af6b1c309b82cba87c0dded6d4199b6f2711d82
Niklas <niklas.mikeler@gmail.com>
2026-08-22 18:13:12 +0200

    feat: add lower bound check for font size
    And connect the border radius to the font size for better scaling

--- src/stores/fontSizeStore.ts
+++ src/stores/fontSizeStore.ts
@@ -7,14 +7,19 @@ interface FontSizeStore {
 
 export const FONT_SIZE_DEFAULT = 1.5;
 
+function lowerBound(fontSize: number) {
+  return Math.max(0.1, fontSize);
+}
+
 export const useFontSizeStore = create<FontSizeStore>((set) => ({
-  fontSize: Number(
-    new URLSearchParams(window.location.search).get("fontSize") || FONT_SIZE_DEFAULT,
+  fontSize: lowerBound(
+    Number(new URLSearchParams(window.location.search).get("fontSize") || FONT_SIZE_DEFAULT),
   ),
   setFontSize: (fontSize) => {
     const url = new URL(window.location.href);
 
-    if (fontSize) {
+    if (fontSize || fontSize === 0) {
+      fontSize = lowerBound(fontSize);
       url.searchParams.set("fontSize", `${fontSize}`);
     } else {
       url.searchParams.delete("fontSize");
